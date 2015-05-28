9afed370ebc384ceb825eb70375ecbb3c3cc946d
Raul Garcia <satanin@gmail.com>
2015-05-28 07:13:22 +0200

    fixing small issues with form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,4 @@ class WelcomeController < ApplicationController
   def index
     @events = Event.where('date >= ?', DateTime.now).order(date: :asc)
   end
-
-  def create
-    @event = Event.postparams
-  end
 end
--- app/views/api/events/index.json.jbuilder
+++ app/views/api/events/index.json.jbuilder
@@ -1,4 +1,5 @@
 json.array! @events do |event|
+  json.id event.id
   json.title event.title
   json.date event.date
   json.link event.link
